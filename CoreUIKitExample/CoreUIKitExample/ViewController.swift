0b55e983bbeb41bee39d291d6533a7b7e474e91c
0vertex <>
2021-10-16 06:39:11 +0530

    Added example project.

---
+++ CoreUIKitExample/CoreUIKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreUIKitExample
+//
+//  Created by Manish on 16/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
