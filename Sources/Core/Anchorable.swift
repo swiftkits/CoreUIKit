779b0c4bbc1fced56de4e8a422451a47f9615ee0
0vertex <89994776+0vertex@users.noreply.github.com>
2023-04-14 15:22:31 +0530

    Refactored anchors docs

--- Sources/Core/Anchorable.swift
+++ Sources/Core/Anchorable.swift
@@ -8,46 +8,107 @@
 import UIKit
 
 public protocol Anchorable {
+    /// Adds current view to provided parent view (if it's not already added)
+    /// - Parameter parentView: a view on which current view (child view) needs to be added
+    /// - Returns: current view
     @discardableResult
     func add(to parentView: UIView) -> Self
     
+    
+    /// Sets `accessibilityIdentifier` on current view
     @discardableResult
     func set(identifier: String) -> Self
     
+    
+    /// Sets leading, trailing, top and bottom anchors equal to parent view's anchors
+    /// - Parameters:
+    ///   - parentView: a view on which current view (child view) needs to be constrained
+    ///   - margin: margin around the constraints
+    /// - Returns: current view
     @discardableResult
     func allAnchorsSame(on parentView: UIView, margin: UIEdgeInsets) -> Self
     
+    
+    /// Sets leading anchor of the current view to provided anchor
+    /// - Parameters:
+    ///   - anchor: anchor value, which will be applied as leading anchor on current view
+    ///   - relation: relation between current view and relative contraint
+    ///   - margin: leading margin / left margin from current view
+    /// - Returns: current view
     @discardableResult
     func leading(with anchor: NSLayoutXAxisAnchor,
                  relation: ConstraintRelation,
                  margin: CGFloat) -> Self
     
+    
+    /// Sets trailing anchor of the current view to provided anchor
+    /// - Parameters:
+    ///   - anchor: anchor value, which will be applied as trailing anchor on current view
+    ///   - relation: relation between current view and relative contraint
+    ///   - margin: trailing margin / right margin from current view
+    /// - Returns: current view
     @discardableResult
     func trailing(with anchor: NSLayoutXAxisAnchor,
                   relation: ConstraintRelation,
                   margin: CGFloat) -> Self
     
+    
+    /// Sets top anchor of the current view to provided anchor
+    /// - Parameters:
+    ///   - anchor: anchor value, which will be applied as top anchor on current view
+    ///   - relation: relation between current view and relative contraint
+    ///   - margin: top margin from current view
+    /// - Returns: current view
     @discardableResult
     func top(with anchor: NSLayoutYAxisAnchor,
              relation: ConstraintRelation,
              margin: CGFloat) -> Self
     
+    
+    
+    /// Sets bottom anchor of the current view to provided anchor
+    /// - Parameters:
+    ///   - anchor: anchor value, which will be applied as bottom anchor on current view
+    ///   - relation: relation between current view and relative contraint
+    ///   - margin: bottom margin from current view
+    /// - Returns: current view
     @discardableResult
     func bottom(with anchor: NSLayoutYAxisAnchor,
                 relation: ConstraintRelation,
                 margin: CGFloat) -> Self
     
+    /// Sets width and heoght anchor with given value on current view
+    /// - Parameters:
+    ///   - width: width value
+    ///   - height: height value
+    /// - Returns: current view
     @discardableResult
     func with(width: CGFloat?, height: CGFloat?) -> Self
     
+    /// Sets centerX anchor of the current view to provided anchor
+    /// - Parameters:
+    ///   - anchor: anchor value, which will be applied as centerX anchor on current view
+    ///   - margin: margin from current view on X-axis
+    /// - Returns: current view
     @discardableResult
     func centerX(with anchor: NSLayoutXAxisAnchor, margin: CGFloat) -> Self
     
+    /// Sets centerY anchor of the current view to provided anchor
+    /// - Parameters:
+    ///   - anchor: anchor value, which will be applied as centerY anchor on current view
+    ///   - margin: margin from current view on Y-axis
+    /// - Returns: current view
     @discardableResult
     func centerY(with anchor: NSLayoutYAxisAnchor, margin: CGFloat) -> Self
     
+    /// Sets center (X & Y) anchor of the current view to provided anchor
+    /// - Parameters:
+    ///   - anchor: anchor value, which will be applied as centerX & centerY anchor on current view
+    ///   - size: with and height value for current view
+    /// - Returns: current view
     @discardableResult
     func center(on parentView: UIView, with size: CGSize) -> Self
     
+    /// Sets identifier for given constraints
     func setConstraintIdentifier(for anchorType: String, for constraint: NSLayoutConstraint)
 }
--- Sources/Extensions/Constraints/UIVIew+Constraints.swift
+++ Sources/Extensions/Constraints/UIVIew+Constraints.swift
@@ -10,9 +10,6 @@ import UIKit
 // MARK: - Add
 extension UIView: Anchorable {
     
-    /// Adds current view to provided parent view (if it's not already added)
-    /// - Parameter parentView: a view on which current view (child view) needs to be added
-    /// - Returns: current view
     @discardableResult
     public func add(to parentView: UIView) -> Self {
         if self.isDescendant(of: parentView) {
@@ -28,7 +25,6 @@ extension UIView: Anchorable {
         return self
     }
     
-    /// Sets `accessibilityIdentifier` on current view
     @discardableResult
     public func set(identifier: String) -> Self {
         self.accessibilityIdentifier = identifier
@@ -40,11 +36,6 @@ extension UIView: Anchorable {
 // MARK: - Constraints
 public extension UIView {
     
-    /// Sets leading, trailing, top and bottom anchors equal to parent view's anchors
-    /// - Parameters:
-    ///   - parentView: a view on which current view (child view) needs to be constrained
-    ///   - margin: margin around the constraints
-    /// - Returns: current view
     @discardableResult
     func allAnchorsSame(on parentView: UIView,
                         margin: UIEdgeInsets = .zero) -> Self {
@@ -56,12 +47,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets leading anchor of the current view to provided anchor
-    /// - Parameters:
-    ///   - anchor: anchor value, which will be applied as leading anchor on current view
-    ///   - relation: relation between current view and relative contraint
-    ///   - margin: leading margin / left margin from current view
-    /// - Returns: current view
     @discardableResult
     func leading(with anchor: NSLayoutXAxisAnchor,
                  relation: ConstraintRelation = .equal,
@@ -74,12 +59,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets trailing anchor of the current view to provided anchor
-    /// - Parameters:
-    ///   - anchor: anchor value, which will be applied as trailing anchor on current view
-    ///   - relation: relation between current view and relative contraint
-    ///   - margin: trailing margin / right margin from current view
-    /// - Returns: current view
     @discardableResult
     func trailing(with anchor: NSLayoutXAxisAnchor,
                   relation: ConstraintRelation = .equal,
@@ -92,12 +71,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets top anchor of the current view to provided anchor
-    /// - Parameters:
-    ///   - anchor: anchor value, which will be applied as top anchor on current view
-    ///   - relation: relation between current view and relative contraint   
-    ///   - margin: top margin from current view
-    /// - Returns: current view
     @discardableResult
     func top(with anchor: NSLayoutYAxisAnchor,
              relation: ConstraintRelation = .equal,
@@ -110,12 +83,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets bottom anchor of the current view to provided anchor
-    /// - Parameters:
-    ///   - anchor: anchor value, which will be applied as bottom anchor on current view
-    ///   - relation: relation between current view and relative contraint
-    ///   - margin: bottom margin from current view
-    /// - Returns: current view
     @discardableResult
     func bottom(with anchor: NSLayoutYAxisAnchor,
                 relation: ConstraintRelation = .equal,
@@ -127,11 +94,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets width and heoght anchor with given value on current view
-    /// - Parameters:
-    ///   - width: width value
-    ///   - height: height value
-    /// - Returns: current view
     @discardableResult
     func with(width: CGFloat? = nil,
               height: CGFloat? = nil) -> Self {
@@ -153,11 +115,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets centerX anchor of the current view to provided anchor
-    /// - Parameters:
-    ///   - anchor: anchor value, which will be applied as centerX anchor on current view
-    ///   - margin: margin from current view on X-axis
-    /// - Returns: current view
     @discardableResult
     func centerX(with anchor: NSLayoutXAxisAnchor,
                  margin: CGFloat = .zero) -> Self {
@@ -169,11 +126,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets centerY anchor of the current view to provided anchor
-    /// - Parameters:
-    ///   - anchor: anchor value, which will be applied as centerY anchor on current view
-    ///   - margin: margin from current view on Y-axis
-    /// - Returns: current view
     @discardableResult
     func centerY(with anchor: NSLayoutYAxisAnchor,
                  margin: CGFloat = .zero) -> Self {
@@ -185,11 +137,6 @@ public extension UIView {
         return self
     }
     
-    /// Sets center (X & Y) anchor of the current view to provided anchor
-    /// - Parameters:
-    ///   - anchor: anchor value, which will be applied as centerX & centerY anchor on current view
-    ///   - size: with and height value for current view
-    /// - Returns: current view
     @discardableResult
     func center(on parentView: UIView,
                 with size: CGSize) -> Self {
