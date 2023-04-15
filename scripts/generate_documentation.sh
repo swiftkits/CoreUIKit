0946bac043e53837a5a9b3a8eeb0c51f576ca8f2
0vertex <89994776+0vertex@users.noreply.github.com>
2023-04-15 10:12:49 +0530

    Added new docc docs

--- scripts/generate_documentation.sh
+++ scripts/generate_documentation.sh
@@ -6,12 +6,12 @@
 #  Created by Manish
 #
 
-xcodebuild docbuild \
--scheme "CoreUIKit" \
--derivedDataPath "../CoreUIKit/docs/doccarchive" \
--destination 'platform=iOS Simulator,name=iPhone 13'
+#xcodebuild docbuild
+#-scheme "CoreUIKit" \
+#-derivedDataPath "../CoreUIKit/docs/doccarchive" \
+#-destination 'platform=iOS Simulator,name=iPhone 13'
 
 $(xcrun --find docc) process-archive \
-transform-for-static-hosting "../CoreUIKit/docs/doccarchive/CoreUIKit.doccarchive" \
+transform-for-static-hosting "../CoreUIKit/build/Release-iphoneos/CoreUIKit.doccarchive" \
 --output-path "../CoreUIKit/docs" \
 --hosting-base-path /
