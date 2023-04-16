e2eae0bb3896814072cb912bfc0d130faa98b49a
0vertex <89994776+0vertex@users.noreply.github.com>
2023-04-16 21:52:51 +0530

    Added missing docs in AnchorPluggable and RelatableAnchor

--- Sources/Core/RelatableAnchor.swift
+++ Sources/Core/RelatableAnchor.swift
@@ -12,10 +12,16 @@ import UIKit
 /// - Let's say view `A` is connected to view `B`, relating same would have some data like `A.leading = B.trailing`
 ///
 public enum RelatableAnchor {
+    /// Leading anchor with relating anchor and data
     case leading(_ relatingView: UIView, _ relatingAnchor: AnchorX = .leading, _ relation: ConstraintRelation = .equal, margin: CGFloat = .zero)
+    /// Trailing anchor with relating anchor and data
     case trailing(_ relatingView: UIView, _ relatingAnchor: AnchorX = .trailing, _ relation: ConstraintRelation = .equal, margin: CGFloat = .zero)
+    /// Top anchor with relating anchor and data
     case top(_ relatingView: UIView, _ relatingAnchor: AnchorY = .top, _ relation: ConstraintRelation = .equal, margin: CGFloat = .zero)
+    /// Bottom anchor with relating anchor and data
     case bottom(_ relatingView: UIView, _ relatingAnchor: AnchorY = .bottom, _ relation: ConstraintRelation = .equal, margin: CGFloat = .zero)
+    /// Width anchor with relating anchor and data
     case width(relation: ConstraintRelation = .equal, _ value: CGFloat)
+    /// Height anchor with relating anchor and data
     case height(relation: ConstraintRelation = .equal, _ value: CGFloat)
 }
